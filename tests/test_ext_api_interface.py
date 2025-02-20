eccdcf7535586fbc3d46a87d1ee6de806d782601
Jrod1302 <jarredreep@gmail.com>
2025-02-20 16:32:39 -0500

    Completed tests for Book_API class.

--- tests/test_ext_api_interface.py
+++ tests/test_ext_api_interface.py
@@ -30,3 +30,34 @@ class TestExtApiInterface(unittest.TestCase):
     def test_get_ebooks(self):
         self.api.make_request = Mock(return_value=self.json_data)
         self.assertEqual(self.api.get_ebooks(self.book), self.books_data)
+    
+    def test_get_ebooks_empty(self):
+        self.api.make_request = Mock(return_value=None)
+        self.assertEqual(self.api.get_ebooks(""), [])
+
+    def test_is_book_available_true(self):
+        self.api.make_request = Mock(return_value={'docs': [{'title': 'Testing Book'}]})
+        self.assertTrue(self.api.is_book_available("Testing Book"))
+
+    def test_is_book_available_false(self):
+        self.api.make_request = Mock(return_value={'docs': []})
+        self.assertFalse(self.api.is_book_available("Testing Book"))
+
+    def test_books_by_author(self):
+        self.api.make_request = Mock(return_value={'docs': [{'title_suggest': 'Testers Biography'}, {'title_suggest': 'Another Book'}]})
+        self.assertEqual(self.api.books_by_author("Jarred Reepmeyer"), ['Testers Biography', 'Another Book'])
+
+    def test_books_by_author_empty(self):
+        self.api.make_request = Mock(return_value=None)
+        self.assertEqual(self.api.books_by_author(""), [])
+
+    def test_get_book_info_full(self):
+        self.api.make_request = Mock(return_value={'docs': [{'title': 'Testers Biography', 'publisher': 'Reepmeyer Publishing', 'publish_year': '2025', 'language': 'English'}]})
+        self.assertEqual(self.api.get_book_info("Testers Biography"), [{'title': 'Testers Biography', 'publisher': 'Reepmeyer Publishing', 'publish_year': '2025', 'language': 'English'}])
+
+    def test_get_book_info_empty(self):
+        self.api.make_request = Mock(return_value=None)
+        self.assertEqual(self.api.get_book_info(""), [])
+
+
+
